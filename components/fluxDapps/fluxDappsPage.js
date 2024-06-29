8769a902a64cb0ed57fa31aa4534687121aaa1d0
Emmanuel Steve <emmanuelsteve43@gmail.com>
2024-06-29 05:54:28 +0100

    update

--- components/fluxDapps/fluxDappsPage.js
+++ components/fluxDapps/fluxDappsPage.js
@@ -29,21 +29,21 @@ export function FluxDappsPage(){
                         <DappTabs/>
                     </div>
                     <div className="row " style={{marginBottom: '100px'}}>
-                        <div className="col-md-4 col-sm-6">
+                        <div className="col-md-4 col-sm-6 mb-3">
                             <div className={styles.card}>
                             <Image src={stars} alt="img1" width={'0'} height={'0'}/>
                             <p>We’ve taken our partnership with @RunOnFlux to the next phase as Flux fine-tunes requirements for running nodes and statistics for node operators. This will allow for a more robust and scalable infrastructure for Kadena’s network!Here’s to a successful partnership!</p>
                             <h6><i>@kadena_io</i></h6>
                             </div>
                         </div>
-                        <div className="col-md-4 col-sm-6">
+                        <div className="col-md-4 col-sm-6 mb-3">
                             <div className={styles.card}>
                             <Image src={stars} alt="img1" width={'0'} height={'0'}/>
                             <p>Today, Flux demonstrated the power of decentralized infrastructure.Through their Kadena node network, they supported #Kaddex when we discovered issues with our nodes.Once we transitioned fully to @RunOnFlux nodes, we saw immediate improvement in performance of the DEX.</p>
                             <h6><i>@KaddeXofficial</i></h6>
                             </div>
                         </div>
-                        <div className="col-md-4 col-sm-6">
+                        <div className="col-md-4 col-sm-6 mb-3">
                             <div className={styles.card}>
                             <Image src={stars} alt="img1" width={'0'} height={'0'}/>
                             <p>We are thrilled to be the first GameFi on Kadena to be running on Flux’s fully decentralized cloud Infrastructure! If you think that decentralization matters and you want to be part of #web3. Don’t settle on your nodes: @RunOnFlux!</p>
