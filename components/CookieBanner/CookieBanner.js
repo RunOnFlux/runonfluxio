3d75230883f541a0c9c0b6b83dc3993ea3d7a20e
JKTUNING <john@j-k-tuning.com>
2024-09-20 07:44:24 -0400

    add linkedin

--- components/CookieBanner/CookieBanner.js
+++ components/CookieBanner/CookieBanner.js
@@ -48,7 +48,7 @@ const CookieBanner = () => {
             src="https://www.googletagmanager.com/gtag/js?id=G-KTKEYE3YMN"
             strategy="afterInteractive" 
           />
-          <Script id="google-analytics">
+          <Script id="google-analytics" strategy="afterInteractive">
             {`
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
@@ -57,6 +57,23 @@ const CookieBanner = () => {
               gtag('config', 'G-KTKEYE3YMN');
             `}
           </Script>
+          <Script id="linkedin" strategy="afterInteractive">
+            {`_linkedin_partner_id = "6548244"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `}
+          </Script>
+          <Script id="linkedin2" strategy="afterInteractive">
+            {`(function(l) { if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]} var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+            `}
+          </Script>
+          <noscript>
+            <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=6548244&fmt=gif" />
+          </noscript>
         </>
       )}
     </>
