b4cdb8c6de9dc972de878d0804f999650ea30ea6
JKTUNING <john@j-k-tuning.com>
2024-10-01 11:52:32 -0400

    adjust retryLimit and timeout

--- utils/contentfulClient.js
+++ utils/contentfulClient.js
@@ -6,6 +6,8 @@ try {
   client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID ?? '',
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? '',
+    retryLimit: 1,
+    timeout: 15000,
   });
 } catch (error) {
   console.error('Error creating Contentful client:', error);
