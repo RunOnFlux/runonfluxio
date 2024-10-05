b7a1f7afe7e34dc9aee39c38516b1096dd5b6f19
JKTUNING <john@j-k-tuning.com>
2024-10-04 23:05:00 -0400

    adjust manage cookies zindex and update packages

--- components/CookieBanner/CookieBanner.js
+++ components/CookieBanner/CookieBanner.js
@@ -24,29 +24,29 @@ const CookieBanner = () => {
       {!manageCookies && (
         <CookieConsent
           style={{ background: "#2B373B", alignItems: "center", flexDirection: "column" }}
-        contentStyle={{ flex: "0 1 auto", justifyContent: "center", margin: "5px" }}
-        location="bottom"
-        buttonText="Accept All"
-        buttonStyle={{ backgroundColor: "#2B61D1", color: "white", fontSize: "13px" }}
-        cookieName="runonflux-consent"         
-        enableDeclineButton
-        declineButtonText="Manage Cookies"
-        declineButtonStyle={{ backgroundColor: "#98A7B9", fontSize: "13px", color: "white" }}
-        declineCookieValue="false"
-        expires={150}
-        flipButtons
-        onAccept={handleAcceptCookies}
-        onDecline={handleDecline}
-      > 
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", maxWidth: "700px" }}>
-          <p>We use cookies to enhance your browsing experience, analyze site traffic, and personalize content. By clicking "Accept All" you consent to our use of cookies to track your experience on our site.</p>
-          <Link href={"/privacyPolicy"} style={{}}>Privacy Policy</Link>
+          contentStyle={{ flex: "0 1 auto", justifyContent: "center", margin: "5px" }}
+          location="bottom"
+          buttonText="Accept All"
+          buttonStyle={{ backgroundColor: "#2B61D1", color: "white", fontSize: "13px" }}
+          cookieName="runonflux-consent"         
+          enableDeclineButton
+          declineButtonText="Manage Cookies"
+          declineButtonStyle={{ backgroundColor: "#98A7B9", fontSize: "13px", color: "white" }}
+          declineCookieValue="false"
+          expires={150}
+          flipButtons
+          onAccept={handleAcceptCookies}
+          onDecline={handleDecline}
+        > 
+          <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", maxWidth: "700px", zIndex: "1000" }}>
+            <p>We use cookies to enhance your browsing experience, analyze site traffic, and personalize content. By clicking "Accept All" you consent to our use of cookies to track your experience on our site.</p>
+            <Link href={"/privacyPolicy"} style={{}}>Privacy Policy</Link>
           </div>        
-          </CookieConsent>
+        </CookieConsent>
       )}
 
       {manageCookies && (
-        <div style={{ position: "sticky", bottom: "0", left: "0", right: "0", backgroundColor: "white", padding: "10px", borderTop: "1px solid #e0e0e0" }}>
+        <div style={{ position: "sticky", bottom: "0", left: "0", right: "0", zIndex: "1000", backgroundColor: "white", padding: "10px", borderTop: "1px solid #e0e0e0" }}>
           <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
             <CookieToggle />
             <button style={{ backgroundColor: "#2B61D1", color: "white", fontSize: "13px", padding: "10px 20px", borderRadius: "5px", marginTop: "10px" }} onClick={() => setManageCookies(false)}>CONFIRM</button>
